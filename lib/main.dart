e9c528de2d9bff4693a22ceefc80e22cd3f366dd
yejin kim <yejin0216@gmail.com>
2021-12-25 18:04:49 +0900

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("텍스트를 쓸 수 있습니다.", style: TextStyle(fontSize: 100))
+      home: SimpleApp()
     );
   }
 }
+
+class SimpleApp extends StatefulWidget {
+  const SimpleApp({Key? key}) : super(key: key);
+
+  @override
+  _SimpleAppState createState() => _SimpleAppState();
+}
+
+class _SimpleAppState extends State<SimpleApp> {
+
+  String box_text = "테스트 텍스트";
+
+  void _updateText()  {
+    setState(() {
+      print("여기는 포인트 영역입니다.");
+      box_text = "박스가 변경되었습니다.";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("this is title"),
+      ),
+      body: Center(
+        child: Text(box_text)
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.create),
+        onPressed: _updateText,
+      ),
+    );
+  }
+}
+
